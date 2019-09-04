d88c23cc0c65beaa1417d0554d7162308d443254
Eugene Han <hanjie1030@gmail.com>
2019-09-04 20:47:24 +0800

    load data

--- dcgan.py
+++ dcgan.py
@@ -16,3 +16,13 @@ from torch.autograd import Variable
 # Set hyperparameters
 batchSize = 64
 imageSize = 64
+
+# Create transformations
+# Create a list of transformations (scaling, tensor conversion, normalization) to apply to the input images.
+transformation = transforms.Compose([transforms.Scale(imageSize), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),]) 
+
+
+# Load dataset
+dataset = dset.CIFAR10(root = './data', download = True, transform = transformation)
+# use dataLoader to get the images of the training set batch by batch.
+dataloader = torch.utils.data.DataLoader(dataset, batch_size = batchSize, shuffle = True, num_workers = 2)
